65bc40a95f1d3be02c5cd1160af2c7d1e3619079
Adrien van den Branden de Reeth <avdbranden@gmail.com>
2016-07-12 12:30:07 +0200

    create games and scores controllers

---
+++ app/controllers/scores_controller.rb
@@ -0,0 +1,4 @@
+class ScoresController < ApplicationController
+  def display
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'games/game'
+  get 'scores/display'
 
-  get 'games/score'
+  get 'games/play'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
